f723e0c4352961295a85639d34d07df769bccd4e
navinthomas94 <navinthomas94@gmail.com>
2022-12-03 11:55:31 -0800

    File removal

--- cert-manager_install.sh
+++
@@ -1,5 +0,0 @@
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.crds.yaml
-kubectl create namespace cert-manager
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm install cert-manager jetstack/cert-manager  --namespace cert-manager  --create-namespace  --version v1.10.1
